adf88189d3236bb2e8040167313c1656c4392d7d
suryavikram2003 <69914435+suryavikram2003@users.noreply.github.com>
2026-05-12 16:34:26 +0530

    Create main App component with routing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Products from './pages/Products'
+import ProductDetail from './pages/ProductDetail'
+import Inquiry from './pages/Inquiry'
+import './index.css'
+
+function App() {
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="/inquiry" element={<Inquiry />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  )
+}
+
+export default App
